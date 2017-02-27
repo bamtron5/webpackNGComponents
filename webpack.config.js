602549ad555249c7d48d1f64afb88771d66909c4
bamtron5 <brandon@brandonam.com>
2017-02-27 12:50:48 -0800

    linter removed

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,32 @@
-var webpack = require('webpack');
-var isProd = JSON.parse(process.env.PROD_ENV || '0'); // `PROD_ENV=1 webpack`
-var analyze = JSON.parse(process.env.ANALYZE || '0');
-var path = require('path');
-var OpenBrowserPlugin = require('open-browser-webpack-plugin');
-var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+var webpack = require('webpack')
+var isProd = JSON.parse(process.env.PROD_ENV || '0') // `PROD_ENV=1 webpack`
+var analyze = JSON.parse(process.env.ANALYZE || '0')
+var path = require('path')
+var OpenBrowserPlugin = require('open-browser-webpack-plugin')
+var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
   entry: {
-    app: "./client/app/app.module.ts"
+    app: './client/app/app.module.ts'
   },
   output: {
-    path: __dirname + "/client/dist",
-    filename: "bundle.js"
+    path: __dirname + '/client/dist',
+    filename: 'bundle.js'
   },
   resolve: {
-    extensions: [".js", ".ts"]
+    extensions: ['.js', '.ts']
   },
   devtool: 'inline-source-map',
   plugins: [
     new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery"
+      $: 'jquery',
+      jQuery: 'jquery'
     }),
     isProd ? new webpack.optimize.UglifyJsPlugin({
       compress: { warnings: false }
-    }) : function(){},
-    analyze ? new BundleAnalyzerPlugin() : function(){},
-    analyze ? new OpenBrowserPlugin({url: '127.0.0.1:8888'}) : function(){}
+    }) : function () {},
+    analyze ? new BundleAnalyzerPlugin() : function () {},
+    analyze ? new OpenBrowserPlugin({url: '127.0.0.1:8888'}) : function () {}
   ],
   module: {
     rules: [
@@ -37,19 +37,19 @@ module.exports = {
       {
         enforce: 'pre',
         test: /\.js$/,
-        loader: "source-map-loader"
+        loader: 'source-map-loader'
       },
       {
         test: /\.scss$/,
         use: [{
-            loader: "style-loader" // creates style nodes from JS strings
+          loader: 'style-loader' // creates style nodes from JS strings
         }, {
-            loader: "css-loader" // translates CSS into CommonJS
+          loader: 'css-loader' // translates CSS into CommonJS
         }, {
-            loader: "sass-loader" // compiles Sass to CSS
+          loader: 'sass-loader' // compiles Sass to CSS
         }]
       },
       { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
     ]
   }
-};
+}
