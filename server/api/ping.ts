1fa0d2f09f46e583450cfeef1db5c8fbfa4d200a
bamtron5 <brandon@brandonam.com>
2017-02-27 09:57:49 -0800

    added toastr

--- server/api/ping.ts
+++ server/api/ping.ts
@@ -2,9 +2,8 @@ import * as express from 'express';
 let router = express.Router();
 
 router.get('/ping', (req, res, next) => {
-  console.log('ok');
-  next({message:'A fake error'});
-  // return res.json({message:'pong'});
+  // next({message:'A fake error'});
+  return res.json({message:'pong'});
 });
 
 export = router;
