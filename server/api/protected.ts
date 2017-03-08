2e1eb94b9229134efe43f0f585ba61f5a0d0e801
bamtron5 <brandon@brandonam.com>
2017-03-07 16:00:19 -0800

    updated to jwt

--- server/api/protected.ts
+++ server/api/protected.ts
@@ -1,10 +1,8 @@
 import * as express from 'express';
-import {isSession} from '../lib/auth';
 import * as passport from 'passport';
 let router = express.Router();
 
 router.get('/protected',
-  isSession,
   passport.authenticate('jwt', {}, (req, res, next) => {
     res.json({message: 'OMG SO SECRET'});
   }));
